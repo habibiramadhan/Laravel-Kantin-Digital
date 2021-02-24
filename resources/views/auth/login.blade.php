65e7c4ee1a2be34fc3ac90de84de55cf5962cce6
misterlengzzz <62778032+misterlengzzz@users.noreply.github.com>
2021-02-24 23:20:25 +0700

    Add view login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,61 @@
-@extends('layouts.app')
+@extends('layouts.login-layout')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+<!-- loader Start -->
+<div id="loading">
+    <div id="loading-center">
     </div>
 </div>
+<!-- loader END -->
+
+<div class="wrapper">
+<section class="login-content">
+   <div class="container h-100">
+      <div class="row align-items-center justify-content-center h-100">
+         <div class="col-md-5">
+            <div class="card">
+               <div class="card-body">
+                  <div class="auth-logo">
+                     <img src="../assets/images/logo-kd.jpeg" class="img-fluid rounded-normal" alt="logo">
+                  </div>
+                  <h2 class="mb-2 text-center">Login Katin Digital</h2>
+                  <p class="text-center">Agar tetap terhubung dengan kami, harap masuk dengan info pribadi Anda.</p>
+                  <form method="POST" action="{{ route('login') }}">
+                @csrf
+                     <div class="row">
+                        <div class="col-lg-12">
+                           <div class="form-group">
+                              <label>Email</label>
+                              <input class="form-control" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus type="email"  placeholder="Email Address">
+                           </div>
+                        </div>
+                        <div class="col-lg-12">
+                           <div class="form-group">
+                              <label>Password</label>
+                              <input class="form-control" name="password" type="password" autocomplete="current-password" placeholder="Password">
+                           </div>
+                        </div>
+                        <div class="col-lg-6">
+                           <div class="custom-control custom-checkbox mb-3">
+                              <input type="checkbox" class="custom-control-input" id="customCheck1">
+                              <label class="custom-control-label" for="customCheck1">Ingat saya</label>
+                           </div>
+                        </div>
+                        <div class="col-lg-6">
+                           <a href="{{ route('password.request') }}" class="text-primary float-right">Lupa Password?</a>
+                        </div>
+                     </div>
+                     <div class="d-flex justify-content-between align-items-center">
+                        <span><a href="auth-sign-up.html" class="text-primary">Daftar Akun</a></span>
+                        <button type="submit" class="btn btn-primary">Masuk</button>
+                     </div>
+                  </form>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+</div>
+
 @endsection
